ec8075b90fa30edb2d3c510ec3ee380ff3e48b37
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2024-12-08 23:52:13 +0900

    Test: 빌드 테스트

--- src/app/writingComplete/layout.tsx
+++ src/app/writingComplete/layout.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-[#C396C2] to-[#7BA1D2]">
+      {children}
+    </div>
+  );
 }
