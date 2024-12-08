bfebcd8411f610be67b1ed1fad3d7d5c910e22ed
k-m-jin <kmjin1105@gmail.com>
2024-12-09 00:57:06 +0900

    fix:css 수정

--- src/app/writingComplete/page.tsx
+++ src/app/writingComplete/page.tsx
@@ -1,13 +1,11 @@
-import WritingCompleteClient from '../../components/writingComplete/WritingCompleteClient';
-import Header from '@/components/header';
+import WritingCompleteClient from "../../components/writingComplete/WritingCompleteClient";
 
 export default function WritingComplete() {
   return (
     <>
-    
-    <div className='bg-gradient-to-b from-[#C396C2] to-[#7BA1D2]'>
-      <Header />
-  <WritingCompleteClient />
-    </div>
-  </>)
+      <div className="h-screen bg-gradient-to-b from-[#C396C2] to-[#7BA1D2]">
+        <WritingCompleteClient />
+      </div>
+    </>
+  );
 }
